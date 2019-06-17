b624886a9584ea4ba5a0fa0780a41b3595736b93
shubham <shrivastav.shub@gmail.com>
2019-06-16 23:25:30 -0700

    Add Comments for complexity

--- moloco_question_1.py
+++ moloco_question_1.py
@@ -7,11 +7,23 @@ Note that x and y can be quite long (each containing millions of characters).
 Implement this function/method and submit a link to your code (answer) below.
 You can share via a link to your code using free websites like https://gist.github.com/ or ideone.com.'''
 
+#Time Complexity O(m+n)
+#m = length of string x
+#n = length of string y 
+
+#Spcace Complexity = O(1)
 class Solution:
     def equalsWhenOneCharRemoved(self, x, y):
+	#############################
+	#Input x:String
+	#Input y:String
+	#Return: Bool (True or False)
+	#############################
         if abs(len(x)-len(y))>1:return False
-        i = j = 0
+        i = j = 0 
+	#i,j iterates string x and y respectively
         dif_count = 0
+	#if difference counter ==1 answer eq True
         while i < len(x) and j < len(y):
             if x[i] != y[j]:
                 dif_count += 1
@@ -20,5 +32,6 @@ class Solution:
                 elif len(x) < len(y):j += 1
                 else:i += 1;j += 1
             else:i += 1 ; j += 1
+        #Check for last characters in the strings
         if i < len(x) or j < len(y): dif_count += 1
         return True if dif_count == 1 else False
