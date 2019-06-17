201ab77028f9cfac3d3d4ec1e355b5a65009029d
shubham <shrivastav.shub@gmail.com>
2019-06-17 00:56:46 -0700

    Moloco question-2

---
+++ data_parse/database.py
@@ -0,0 +1,8 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join('..', 'data_parse')))
+read_file = open("sample_data_question_2.txt","r")
+
+
+class Database:
+    def read(self):
+        return read_file.readlines()
